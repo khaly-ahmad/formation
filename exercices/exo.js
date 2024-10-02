c8381e2d1c261ce81cee34ee41b6deb03f7f4db7
Khaly Ahmad <khalyamad@icloud.com>
2024-10-02 22:39:00 +0200

    express packages

--- exercices/exo.js
+++ exercices/exo.js
@@ -3,10 +3,18 @@ document.addEventListener('scroll', () => {
     items.forEach((item) => {
         const positionObject = item.getBoundingClientRect().top;
         const screenPosition = window.innerHeight - 100;
-        if (positionObject < screenPosition || positionObject<200 ) {
+        if (positionObject < screenPosition) {
             item.classList.add("item1")
         } else {
             item.classList.remove("item1")
         }
+    })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cursor = document.querySelector('.cursor');
+    document.addEventListener('mousemove', (e) => {
+        cursor.style.left = `${e.pageX}px`;
+        cursor.style.top = `${e.pageY}px`;
     })
 })
